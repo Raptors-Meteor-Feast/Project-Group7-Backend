fb7486edc20759f1e6633fded08e4c6695ee648d
SundayNT <azterisk13@gmail.com>
2024-12-07 23:50:38 +0700

    test auth middleware from cart API

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { addToCart, deleteGameInCart, getUserCart } from "../controllers/cartController.js";
+import authUser from "../middleware/auth.js";
 
 const cartRouter = express.Router();
 
-cartRouter.get("/get", getUserCart);
-cartRouter.post("/add", addToCart);
-cartRouter.delete("/delete", deleteGameInCart);
+cartRouter.get("/get", authUser, getUserCart);
+cartRouter.post("/add", authUser, addToCart);
+cartRouter.delete("/delete", authUser, deleteGameInCart);
 
 export default cartRouter;
