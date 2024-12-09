472f9fb4e91dcb276ee47c42f78bbe50a9152beb
SIWAKRIT PATTAVEESIN <mr.siwakrit@gmail.com>
2024-12-10 02:08:45 +0700

    update comment

--- models/userModel.js
+++ models/userModel.js
@@ -5,8 +5,8 @@ const userSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     displayName: { type: String, required: true },
-    email: { type: String, required: true, unique: true, lowercase: true },
-    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true, lowercase: true, match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ },
+    password: { type: String, required: true , minlength: 8 },
     resetPasswordToken: { type: String }, // Token สําหรับรีเซ็ตรหัสผ่าน
     resetPasswordExpires: { type: Date }, // วันหมดอายุของ Token สําหรับรีเซ็ตรหัสผ่าน
     cartData: { type: Object, default: {} }
