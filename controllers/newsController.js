f12fbe33d2e3bc384e8729b74d5b4a94448c40b9
Kulsayas <Kulsaya.see@gmail.com>
2024-12-10 21:33:09 +0700

    add newsController and NewsRoute

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -3,8 +3,8 @@ import newsModel from "../models/newsModel.js";
 export const getNews = async (req, res) => {
   try {
     const news = await newsModel.find();
-    res.status(200).send({
-      news: news,
+    res.status(200).json({
+      data: news,
     });
   } catch (error) {
     res.status(500).send({
@@ -18,14 +18,14 @@ export const getNews = async (req, res) => {
 export const getNewsById = async (req, res) => {
   try {
     const id = req.params.newsId;
-    const detailNews = await newsModel.findById(id);
-    res.status(200).json({
-      data: detailNews,
-    });
+    let detailNews = await newsModel.findById(id);
+    if (!detailNews) {
+      return res.status(400).send("Detail game not found");
+    } else {
+      res.json({ data: detailNews });
+    }
   } catch (error) {
-    res.status(500).send({
-      status: "failure",
-      message: "Internal server error.",
-    });
+    console.log(error);
+    res.json({ success: false, message: error.message });
   }
 };
